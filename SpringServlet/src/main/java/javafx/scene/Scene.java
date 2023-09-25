0aff5c1f258941b20a8d0a888bae6aeb106a58b1
amanopyadav <amanopyadav@gmail.com>
2023-09-25 13:27:55 +0530

    SpringMvcApp

---
+++ SpringServlet/src/main/java/javafx/scene/Scene.java
@@ -0,0 +1,15 @@
+package javafx.scene;
+
+public class Scene {
+
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
